65ee1f7f0d4870899bec1255f848611a23137986
CrosFIght <158484871+CrosFIght@users.noreply.github.com>
2024-03-27 18:30:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 import discord
+from discord.ext import commands
 from settings import settings
 from bot_logic import gen_pass, Coin
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'We logged in as {client.user}')
+    print(f'We logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('Hello'): 
-        await message.channel.send("Hello, human being! How nice of you to contact me. What can I do for you today?")
-    elif message.content.startswith('$Password'):
-        await message.channel.send(f'Your password: \n||{gen_pass(10)}||')
-    elif message.content.startswith('$Flip_coin'):
-        await message.channel.send(f'Side of a coin: \n{Coin()}')
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\\U0001f642")
-    elif  message.content.startswith('$heh'):
-        if len(message.content) > 4:
-            count_heh = int(message.content[4:])
-        else:
-            count_heh = 5
-        await message.channel.send("he" * count_heh)
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello, human being! How nice of you to contact me. What can I do for you today?")
 
-client.run(settings)
+@bot.command()
+async def password(ctx):
+    await ctx.send(f'Your password: \n||{gen_pass(10)}||')
+
+@bot.command()
+async def coin_flip(ctx):
+    await ctx.send(f'Side of a coin: \n{Coin()}')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+#@bot.command()
+#async def online(ctx):
+    #online_members = sum(member.status == discord.Status.online for member in ctx.guild.members)
+    #await ctx.send("Online: " + online_members)
+
+bot.run(settings)
